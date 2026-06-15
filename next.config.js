a14d518f99970da939588509a71d850a6d8dfcc0
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-15 21:55:52 +0630

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "" : "",
+  assetPrefix: isProd ? "" : "",
+};
